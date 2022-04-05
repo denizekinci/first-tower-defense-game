dd9f8018fd46e29f0f8899e4b08bd54112f96727
denizekinci <deniz.ekinci.dev@gmail.com>
2022-04-05 18:45:53 +0300

    Start-end points, spawner, turret, shooting added.

---
+++ Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isBuilt = false;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# first-tower-defense-game
